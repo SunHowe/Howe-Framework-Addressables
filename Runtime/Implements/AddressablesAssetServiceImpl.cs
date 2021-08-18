86b25324ad628b6087a508f7224b53a436704ebb
SunHowe <tosunhowe@gmail.com>
2021-08-18 18:02:05 +0800

    addressables service

---
+++ Runtime/Implements/AddressablesAssetServiceImpl.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using HoweFramework.Asset;
+using UnityEngine;
+
+namespace HoweFramework.Addressables
+{
+    /// <summary>
+    /// 基于Unity-Addressables实现的资产服务
+    /// </summary>
+    public class AddressablesAssetServiceImpl : IAssetService
+    {
+        public async Task<T> LoadAsync<T>(string resKey)
+        {
+            return await UnityEngine.AddressableAssets.Addressables.LoadAssetAsync<T>(resKey).Task;
+        }
+
+        public async Task<GameObject> InstantiateAsync(string resKey)
+        {
+            return await UnityEngine.AddressableAssets.Addressables.InstantiateAsync(resKey).Task;
+        }
+
+        public T LoadSync<T>(string resKey)
+        {
+            return UnityEngine.AddressableAssets.Addressables.LoadAssetAsync<T>(resKey).WaitForCompletion();
+        }
+
+        public GameObject InstantiateSync(string resKey)
+        {
+            return UnityEngine.AddressableAssets.Addressables.InstantiateAsync(resKey).WaitForCompletion();
+        }
+
+        public void Release<T>(T asset)
+        {
+            UnityEngine.AddressableAssets.Addressables.Release();
+        }
+
+        public void Release(GameObject gameObject)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
